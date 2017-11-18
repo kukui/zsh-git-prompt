ae7a56c573fa77dcc0914139e99b528bc4b3eccd
kai keliikuli <kai.keliikuli@gmail.com>
2017-11-18 13:31:52 -0800

    updates

--- zshrc.sh
+++ zshrc.sh
@@ -95,14 +95,14 @@ git_super_status() {
 # Default values for the appearance of the prompt. Configure at will.
 ZSH_THEME_GIT_PROMPT_PREFIX="("
 ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{●%G%}"
-ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖%G%}"
-ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{✚%G%}"
+ZSH_THEME_GIT_PROMPT_SEPARATOR=" | "
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$FG[142]%}"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$FG[130]%}%{●%G%}"
+ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$FG[124]%}%{✖%G%}"
+ZSH_THEME_GIT_PROMPT_CHANGED="%{$FG[074]%}%{✚%G%}"
 ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
 ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{…%G%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$FG[green]%}%{✔%G%}"
 
 
